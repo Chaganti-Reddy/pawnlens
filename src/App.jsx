3fd815b27a3a20c73f56498f58dccce0f5bcddc6
Chaganti-Reddy <chagantivenkataramireddy4@gmail.com>
2026-07-28 13:20:00 +0530

    Routes, nav, strings and styles for tactics and the fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home.jsx';
 import Review from './pages/Review.jsx';
 import Weakness from './pages/Weakness.jsx';
 import Train from './pages/Train.jsx';
+import Tactics from './pages/Tactics.jsx';
 import Drills from './pages/Drills.jsx';
 import OpeningsDrill from './pages/OpeningsDrill.jsx';
 import './App.css';
@@ -21,6 +22,7 @@ export default function App() {
               <Route path="/" element={<Home />} />
               <Route path="/review" element={<Review />} />
               <Route path="/train" element={<Train />} />
+              <Route path="/tactics" element={<Tactics />} />
               <Route path="/drills" element={<Drills />} />
               <Route path="/openings" element={<OpeningsDrill />} />
               <Route path="/weaknesses" element={<Weakness />} />
